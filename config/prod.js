6edb4954a7d63c7a24dae4e859e4916e1b855d7f
Phillip Littman <srpg4ever@gmail.com>
2018-10-02 13:15:28 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+// prod.js - production keys here
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+};
+
+// prod
+// auth js origin: https://mighty-brushlands-37928.herokuapp.com/
+// auth rd uri: https://mighty-brushlands-37928.herokuapp.com/auth/google/callback 
+// clientID: 1097175171651-pcqvt01ltilut476ipot98ve9l88ejud.apps.googleusercontent.com
+// clientSecret: 8DMZQMwZ3VlBUECZrvK00wP7
